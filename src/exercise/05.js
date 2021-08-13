788c7e2c257ed53f4d4b5d02577c09188d46252b
John Gedeon <john@truss.works>
2021-08-13 15:03:08 -0700

    Complete exercise 5 extra 2

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -6,12 +6,17 @@ import {Switch} from '../switch'
 
 const callAll = (...fns) => (...args) => fns.forEach(fn => fn?.(...args))
 
+const actions = {
+  toggle: 'toggle',
+  reset: 'reset',
+}
+
 function toggleReducer(state, {type, initialState}) {
   switch (type) {
-    case 'toggle': {
+    case actions.toggle: {
       return {on: !state.on}
     }
-    case 'reset': {
+    case actions.reset: {
       return initialState
     }
     default: {
@@ -25,8 +30,8 @@ function useToggle({initialOn = false, reducer = toggleReducer} = {}) {
   const [state, dispatch] = React.useReducer(reducer, initialState)
   const {on} = state
 
-  const toggle = () => dispatch({type: 'toggle'})
-  const reset = () => dispatch({type: 'reset', initialState})
+  const toggle = () => dispatch({type: actions.toggle})
+  const reset = () => dispatch({type: actions.reset, initialState})
 
   function getTogglerProps({onClick, ...props} = {}) {
     return {
@@ -57,7 +62,7 @@ function App() {
   const clickedTooMuch = timesClicked >= 4
 
   function toggleStateReducer(state, action) {
-    if (action.type === 'toggle' && timesClicked >= 4) {
+    if (action.type === actions.toggle && timesClicked >= 4) {
       return {on: state.on}
     }
     return toggleReducer(state, action)
